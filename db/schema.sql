5ac54204971f66eb64990ff78b456c4f3fa656c0
darmstrong <armstrong2579@gmail.com>
2022-07-26 17:53:11 -0700

    add base database and tables in schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_list_db;
+CREATE DATABASE employee_list_db;
+
+USE employee_list_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    dept_name VARCHAR(30)
+);
+
+DROP TABLE IF EXISTS employee_role;
+CREATE TABLE employee_role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30)
+    salary DECIMAL,
+    dept_id INT --take on that in dept table
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id , -- hold reference to employee role
+    manager_id -- hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+);
