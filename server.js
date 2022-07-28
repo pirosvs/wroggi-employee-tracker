8d57088388400c1af22905defe1ea0de422da998
darmstrong <armstrong2579@gmail.com>
2022-07-27 22:32:31 -0700

    add structure into routes

--- server.js
+++ server.js
@@ -87,16 +87,66 @@ const updateQuestions = [
 ];
 
 // route to get depts
-app.get()
+app.get('/api/dept', async (req, res) => {
+  try {
+    res.status(200).json(userData);
+  } catch (err) {
+      res.json(err);
+  }
+})
 // route to get roles
-app.get()
+app.get('/api/roles', async (req, res) => {
+  try {
+    res.status(200).json(userData);
+  } catch (err) {
+      res.json(err);
+  }
+})
 // route to get employees
-app.get()
+app.get('/api/employees', async (req, res) => {
+  try {
+    res.status(200).json(userData);
+  } catch (err) {
+      res.json(err);
+  }
+})
 // route to post dept
-app.post()
+app.post('/api/dept', async (req, res) => {
+  try {
+    res.status(200).json(userData);
+  } catch (err) {
+      res.json(err);
+  }
+})
 // route to post role
-app.post()
+app.post('/api/roles', async (req, res) => {
+  try {
+    res.status(200).json(userData);
+  } catch (err) {
+      res.json(err);
+  }
+})
 // route to post employee
-app.post()
+app.post('/api/employees', async (req, res) => {
+  try {
+    res.status(200).json(userData);
+  } catch (err) {
+      res.json(err);
+  }
+})
+
 // route to put/ update employee
-app.put()
+app.put('/api/amployee/:id', async (req, res) => {
+  // need to specify we're updating 
+  try {
+    const userData = await employee.update(req.role_id, {
+      where: {
+        id: req.params.id,
+      },
+    },
+    res.status(200).json(userData)
+  );
+  } catch (err) {
+      res.json(err);
+  }
+});
