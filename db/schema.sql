b4f0e7baba4219ece4a47258a2e5111e56d6822d
darmstrong <armstrong2579@gmail.com>
2022-07-27 21:56:11 -0700

    auto increment ids

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ USE employee_list_db;
 
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT AUTO INCREMENT PRIMARY KEY,
     dept_name VARCHAR(30)
 );
 
 DROP TABLE IF EXISTS employee_role;
 CREATE TABLE employee_role (
-    id INT PRIMARY KEY,
+    id INT AUTO INCREMENT PRIMARY KEY,
     title VARCHAR(30)
     salary DECIMAL,
     dept_id INT --take on that in dept table
@@ -19,7 +19,7 @@ CREATE TABLE employee_role (
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id , -- hold reference to employee role
