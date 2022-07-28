f91a33543d94556e2bd095e6cb1808f6139c45b3
darmstrong <armstrong2579@gmail.com>
2022-07-27 21:34:10 -0700

    add basic set up for questions and routes

--- server.js
+++ server.js
@@ -15,3 +15,25 @@ app.use(express.static('public'));
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
+// add dept quetions
+const addDeptQuestions = [];
+// add role questions 
+const addRoleQuestions = [];
+// add employee questions
+const addEmployeeQuestions = [];
+
+// route to get depts
+app.get()
+// route to get roles
+app.get()
+// route to get employees
+app.get()
+// route to post dept
+app.post()
+// route to post role
+app.post()
+// route to post employee
+app.post()
+// route to put/ update employee
+app.put()
